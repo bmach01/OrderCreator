955a9b407a109f68d18e12761ef23a3314dcbb64
Bartosz Machniak <b.machniak01@gmail.com>
2024-12-10 23:51:17 +0100

    Implemented OrderRepository. Made it use prop database txt file and json formatting for saving/reading.

--- OrderCreator/DAL/OrderRepository.cs
+++ OrderCreator/DAL/OrderRepository.cs
@@ -4,23 +4,72 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace OrderCreator.DAL
 {
     internal class OrderRepository : IOrderRepository
     {
-        public OrderRepository() { }
+        private static readonly string _dbPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "OrderCreatorDBProp.txt"
+        );
+
+        public OrderRepository() 
+        {
+            if (!File.Exists(_dbPath))
+            {
+                using (File.Create(_dbPath)) { }
+            }
+        }
 
         public ReadOnlyCollection<Order> GetPreviousOrders()
         {
-            // Get saved orders to present
-            return new List<Order>().AsReadOnly();
+            try
+            {
+                string fileContent = File.ReadAllText(_dbPath);
+
+                if (string.IsNullOrWhiteSpace(fileContent))
+                {
+                    return new ReadOnlyCollection<Order>(new List<Order>());
+                }
+
+                var orders = JsonSerializer.Deserialize<List<Order>>(fileContent);
+                return orders?.AsReadOnly() ?? new ReadOnlyCollection<Order>(new List<Order>());
+            }
+            catch (Exception ex)
+            {
+                throw new IOException("Failed to retrieve previous orders.", ex);
+            }
         }
 
         public void SaveOrder(Order order)
         {
-            // Here order would be sent to the server
+            // Here order would be normally sent to the server
+            // For the sake of the exercise, it will be turned to JSON and stored in text file
+            try
+            {
+                List<Order> orders;
+
+                if (File.Exists(_dbPath))
+                {
+                    string fileContent = File.ReadAllText(_dbPath);
+                    orders = string.IsNullOrWhiteSpace(fileContent)
+                        ? new List<Order>()
+                        : JsonSerializer.Deserialize<List<Order>>(fileContent) ?? new List<Order>();
+                }
+                else
+                {
+                    orders = new List<Order>();
+                }
+                orders.Add(order);
+
+                File.WriteAllText(_dbPath, JsonSerializer.Serialize(orders));
+            }
+            catch (Exception ex)
+            {
+                throw new IOException("Failed to save the order.", ex);
+            }
         }
     }
 }
